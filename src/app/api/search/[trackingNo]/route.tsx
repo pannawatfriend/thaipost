27297ec379dff676fc32124f607069951d8f29c3
pannawatfriend <friendlysripa@gmail.com>
2024-09-26 02:47:28 +0700

    chore: Add font loading and image retrieval in search route

--- src/app/api/search/[trackingNo]/route.tsx
+++ src/app/api/search/[trackingNo]/route.tsx
@@ -2,16 +2,36 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { NextResponse } from "next/server"
 import { ImageResponse } from "next/og"
 import MainImage from "../../../components/MainImage/index"
+export const runtime = "edge"
+
+const loadFont = async () => {
+    return fetch(
+        new URL("../../../../../public/fonts/Kanit/Kanit-Light.ttf", import.meta.url)
+    ).then((r) => r.arrayBuffer())
+}
 
 export async function GET(req: Request) {
+    const KanitLigth = await loadFont()
     // const body = await req.json()
 
     // const all = await convertHTML(JSON.stringify(body))
     // res.setHeader("Content-Type", "image/png")
 
+    // const imgdata = await fetch(
+    //     new URL("../../../../public/images/barcode.png", import.meta.url)
+    // ).then((r) => r.arrayBuffer())
+
+    
+
     return new ImageResponse(<MainImage />, {
         width: 700,
         height: 700,
+        fonts: [
+            {
+                name: "Kanit-Light",
+                data: KanitLigth,
+            }
+        ],
         status: 200,
         statusText: "Ok",
         headers: {
